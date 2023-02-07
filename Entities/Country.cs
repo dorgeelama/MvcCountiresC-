ad0a7ea873fe303aa8112b0ed1f8dc1d9458ea0a
dorgee <dorgeelama91@gmail.com>
2023-02-07 10:30:51 -0700

    added service contracts and configured dependencies

---
+++ Entities/Country.cs
@@ -0,0 +1,12 @@
+﻿namespace Entities
+{
+    public class Country
+    {
+        
+        /// <summary>
+        /// Domain Model for storing country details
+        /// </summary>
+      public Guid CountryID { get; set; }
+      public string? CountryName { get; set; }
+    }
+}
---
+++ ServiceContracts/DTO/CountryAddRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Entities;
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// DTO class for adding new country
+    /// </summary>
+    public class CountryAddRequest
+    {
+        // we do not include id here becuase when the user makes a request 
+        // they will only send country name
+        // only after adding country to the list should the id be generated
+        public string? CountryName { get; set; }
+
+        // after validating this country name if it is correct 
+        // we have to convert the same into a country object inorder to add it to the acutal data source
+        // have to convert from country add request to country object
+
+        public Country ToCountry()
+        {
+            return new Country()
+            {
+                CountryName = CountryName
+            };
+        }
+    }
+}
