ce77c43c2e0d3748ad6ed71bf03989be9fbb6f71
dorgee <dorgeelama91@gmail.com>
2023-02-07 11:07:05 -0700

    added services library

---
+++ CRUDTests/CountriesServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using ServiceContracts;
+using System;
+using System.Collections.Generic;
+using Entities;
+using ServiceContracts.DTO;
+
+namespace CRUDTests
+{
+    public class CountriesServiceTest
+    {
+        private readonly ICountriesService _countriesService;
+
+        public CountriesServiceTest()
+        {
+            
+        }
+    }
+}
