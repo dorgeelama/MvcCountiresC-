50dcf527c6d6330538a2f2bf344aa56e68213b56
dorgee <dorgeelama91@gmail.com>
2023-02-07 11:00:59 -0700

    add method signature for ICountriesService interface

--- ServiceContracts/ICountriesService.cs
+++ ServiceContracts/ICountriesService.cs
@@ -1,7 +1,19 @@
-﻿namespace ServiceContracts
+﻿using ServiceContracts.DTO;
+
+namespace ServiceContracts
 {
-    public class ICountriesService
+    /// <summary>
+    /// Represents business logic for manipulating Country entity
+    /// </summary>
+    public interface ICountriesService
     {
+        // takes in country add request and return a country response
+        /// <summary>
+        /// Adds a country object to the list of countries
+        /// </summary>
+        /// <param name="countryAddRequest">Country object to be added</param>
+        /// <returns>Returns the country object after adding it (Including newly generated country id)</returns>
+        CountryResponse AddCountry(CountryAddRequest? countryAddRequest);
 
     }
 }
