8ab9a729c9e933228df4c738f14151405522e56a
dorgee <dorgeelama91@gmail.com>
2023-03-01 14:23:52 -0700

    Searching functionality finished

--- CRUDExample/Controllers/PersonsController.cs
+++ CRUDExample/Controllers/PersonsController.cs
@@ -15,9 +15,23 @@ namespace CRUDExample.Controllers
 
         [Route("persons/index")]
         [Route("/")]
-        public IActionResult Index()
+        public IActionResult Index(string searchBy, string? searchString)
         {
-            List<PersonResponse> persons = _personService.GetAllPersons();
+            ViewBag.SearchFields = new Dictionary<string, string>()
+      {
+        { nameof(PersonResponse.PersonName), "Person Name" },
+        { nameof(PersonResponse.Email), "Email" },
+        { nameof(PersonResponse.DateOfBirth), "Date of Birth" },
+        { nameof(PersonResponse.Gender), "Gender" },
+        { nameof(PersonResponse.CountryID), "Country" },
+        { nameof(PersonResponse.Address), "Address" }
+      };
+
+
+
+            List<PersonResponse> persons = _personService.GetFilteredPersons(searchBy, searchString);
+            ViewBag.CurrentSearchBy = searchBy;
+            ViewBag.CurrentSearchString = searchString;
             return View(persons);
         }
     }
